2c4f00051c96da6ce9d2f4f9d7304d7702072a52
Henos yifru <Yifruhenos@gmail.com>
2023-09-15 11:12:56 -0700

    Multiple mains2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,23 +1,17 @@
 #include "main.h"
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-
-void positive_or_negative(int i);
-
-#endif /* MAIN_H */
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {
-int i;
+ int i;
 
-i = 98;
-positive_or_negative(i);
+ i = 98;
+ positive_or_negative(i);
 
-return (0);
+ return (0);
 }
+
